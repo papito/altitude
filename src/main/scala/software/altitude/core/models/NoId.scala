bbefbcd53a167ebfb4ec067412581e763c5d139d
Andrei Taranchenko <drey10@gmail.com>
2025-08-12 20:52:10 -0400

    Adding Mill/Tapir project placeholders. Removing the old src folder.

--- src/main/scala/software/altitude/core/models/NoId.scala
+++
@@ -1,6 +0,0 @@
-package software.altitude.core.models
-
-/** Trait to use for submodels and transient objects that are not stored in databases as separate entities. */
-trait NoId {
-  val id: Option[String] = None
-}
