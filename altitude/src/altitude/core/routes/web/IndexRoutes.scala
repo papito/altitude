5a9c94d552e831d1c64bd5c162a1eeba9d0ba047
Andrei Taranchenko <drey10@gmail.com>
2026-02-05 04:43:00 -0500

    Setup form shows up but does not work yet.

--- altitude/src/altitude/core/routes/web/IndexRoutes.scala
+++ altitude/src/altitude/core/routes/web/IndexRoutes.scala
@@ -1,15 +1,28 @@
 package altitude.core.routes.web
 
+import altitude.core.App
 import org.slf4j.Logger
 
 class IndexRoutes(using logger: Logger) extends cask.Routes:
-  @cask.get(s"/")
+  @cask.staticFiles("/static/")
+  def staticFileRoutes() = "altitude/static"
+
+  @cask.get("/")
   def index(): cask.Response[String] = {
+    if (!App.altitude.isInitialized) {
+      logger.warn("App is not initialized, redirecting to setup")
+      return cask.Redirect("/setup")
+    }
+
     val payload = "<!doctype html>" + html.index()
     cask.Response(payload, 200, Seq(("Content-Type", "text/html")))
   }
 
-  @cask.staticFiles("/static/")
-  def staticFileRoutes() = "altitude/static"
+  @cask.get("/setup")
+  def setup(): cask.Response[String] = {
+    val payload = "<!doctype html>" + html.setup()
+    cask.Response(payload, 200, Seq(("Content-Type", "text/html")))
+  }
+
 
   initialize()
