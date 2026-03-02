c37810655f5c09aa52bc4f2bfcdd6052b245ed93
Andrei Taranchenko <drey10@gmail.com>
2026-03-02 17:27:55 -0500

    More tuning for face rec and removing dead code.

--- altitude/src/altitude/core/service/FaceRecognitionService.scala
+++ altitude/src/altitude/core/service/FaceRecognitionService.scala
@@ -9,29 +9,9 @@ import altitude.core.models.Face
 import altitude.core.models.FaceImages
 import altitude.core.models.Person
 import altitude.core.transactions.TransactionManager
-import org.apache.pekko.actor.typed.Scheduler
-import org.apache.pekko.util.Timeout
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.duration.DurationInt
-
-object FaceRecognitionService {
-
-  /**
-   * If there is no machine learning model verified hit, we cycle through all people in the database, but only doing the matching
-   * on THIS many best face detections that we have (1 to X)
-   *
-   * Higher number means more matches will be found, at the cost of performance.
-   *
-   * Lower number means faster matching but the same person may be detected as new. Technically, just 1 "top" face will work, and
-   * the accuracy benefits get diminished the higher we go
-   */
-  val MAX_COMPARISONS_PER_PERSON = 12
-
-  /** If the cosine distance between the facial features is below this threshold, we consider the face a match. */
-  val COSINE_DISTANCE_THRESHOLD = .63
-}
 
 class FaceRecognitionService(val app: Altitude) {
   final val logger: Logger = LoggerFactory.getLogger(getClass)
